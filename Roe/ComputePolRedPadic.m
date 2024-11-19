479f143def07c53df62bd67ec8c35a6d2d0cebc9
David Roe <roed.math@gmail.com>
2024-11-18 22:40:48 -0500

    Script for computing PolRedPadic

---
+++ Roe/ComputePolRedPadic.m
@@ -0,0 +1,18 @@
+// Usage: ls /scratch/lf/poly_2_8/ | parallel -j100 --timeout 600 "magma -b p:=2 n:=8 fname:={0} ComputePolRedPadic> /scratch/lf/err_2_8/{0}"
+
+Attach("../Pauli/polredabs.m");
+SetColumns(0);
+
+infile := Sprintf("/scratch/lf/poly_%o_%o/" * fname, p, n);
+outfile := Sprintf("/scratch/lf/out_%o_%o/" * fname, p, n);
+p := StringToInteger(p);
+polys = Split(Read(fname), "\n");
+R<x> := PolynomialRing(pAdicRing(p, 30));
+Zx := PolynomialRing(Integers());
+AssignNames(~Zx, "x");
+for poly in polys do
+    f := eval poly;
+    bundle := PolRedPadic(f);
+    PrintFile(outfile, Sprintf("%o|%o", poly, Join([Sprint(g) : g in bundle], ",")));
+end for;
+quit;
