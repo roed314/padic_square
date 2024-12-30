758a924052312c749bb5a1dbd4bd45c15c4ddfa3
David Roe <roed.math@gmail.com>
2024-12-30 18:54:36 -0500

    Split by phi0 and a, use want_filter:=false

--- Roe/ComputeAllExtensions.m
+++ Roe/ComputeAllExtensions.m
@@ -18,7 +18,7 @@ p := StringToInteger(pieces[1]);
 f := StringToInteger(pieces[2]);
 e := StringToInteger(pieces[3]);
 c := StringToInteger(pieces[4]);
-RP := eval Read(infile);
+// RP := eval Read(infile); // before switch to reading phi0 and a
 
 Zx<x> := PolynomialRing(Integers());
 prec := 64;
@@ -32,26 +32,25 @@ while true do
         end if;
         RK, res := ResidueClassField(k);
         RKz<z> := PolynomialRing(RK);
-        phi0s := DistinctConstantCoefficients(k, e);
-        for phi0 in phi0s do
-            A := AllResidualPolynomials(k, RP, phi0);
-            for a in A do
-                for relpoly in AllTotallyRamifiedExtensions(k, RP, a, res(phi0/Prime(k))) do
-                    if f eq 1 then
-                        abspoly := Zx!relpoly;
-                    else
-                        B<b> := TotallyRamifiedExtension(k, relpoly);
-                        Bx<x> := PolynomialRing(B);
-                        y := Roots(Bx!DefiningPolynomial(k) - b, B)[1][1];
-                        abspoly := Zx!MinimalPolynomial(y,k0);
-                    end if;
-                    PrintFile(outfile, sprint(abspoly));
-                    //R<x> := PolynomialRing(k0);
-                    //bundle := PolRedPadic(R!abspoly);
-                    //best := Distinguished(bundle);
-                    //PrintFile(outfile, Sprintf("%o|%o|%o", sprint(abspoly), sprint(Zx!best), Join([sprint(Zx!g) : g in bundle], ", ")));
-                end for;
-            end for;
+        //phi0s := DistinctConstantCoefficients(k, e);
+        //for phi0 in phi0s do
+        //    A := AllResidualPolynomials(k, RP, phi0);
+        //    for a in A do
+        RP, phi0, a := Explode(eval Read(infile));
+        for relpoly in AllTotallyRamifiedExtensions(k, RP, a, res(phi0/Prime(k)) : want_filter:=false) do
+            if f eq 1 then
+                abspoly := Zx!relpoly;
+            else
+                B<b> := TotallyRamifiedExtension(k, relpoly);
+                Bx<x> := PolynomialRing(B);
+                y := Roots(Bx!DefiningPolynomial(k) - b, B)[1][1];
+                abspoly := Zx!MinimalPolynomial(y,k0);
+            end if;
+            //PrintFile(outfile, sprint(abspoly));
+            R<x> := PolynomialRing(k0);
+            bundle := PolRedPadic(R!abspoly);
+            best := Distinguished(bundle);
+            PrintFile(outfile, Sprintf("%o|%o|%o", sprint(abspoly), sprint(Zx!best), Join([sprint(Zx!g) : g in bundle], ", ")));
         end for;
         break;
     catch err
