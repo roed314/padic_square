0a70ab864ea37459e5ecbdb3e9ec58849977a799
David Roe <roed.math@gmail.com>
2025-05-11 15:10:01 -0400

    Script for computing autsize and disc for debugging p=3,n=18,f=3 case

---
+++ Roe/AutDisc.m
@@ -0,0 +1,22 @@
+// Usage: ls /scratch/lf/filtered | parallel -j80 -- timeout 300 --results /scratch/lf/adout/ "magma -b fname:={0} AutDisc.m"
+
+Attach("../Pauli/polredabs.m");
+SetColumns(0);
+
+infile := "/scratch/lf/filtered/" * fname;
+outfile := "/scratch/lf/outdisc/" * fname;
+
+s := Read(infile);
+abspoly, relpoly := Explode(Split(s, "|"));
+
+p, n, f, i := Explode([StringToInteger(c) : c in Split(label, ".")]);
+prec := 4*n;
+K0 := pAdicRing(p, prec);
+K := UnramifiedExtension(K0, conway_or_jr_polynomial(K0, f));
+Rrel<x> := PolynomialRing(K);
+relpoly := eval relpoly;
+L := TotallyRamifiedExtension(K, relpoly);
+c := Valuation(Discriminant(L, K0));
+A := AutomorphismGroup(L, K0);
+PrintFile(outfile, Sprintf("%o|%o", c, #A));
+quit;
